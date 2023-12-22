212c491a8990d735194c78aa24a7ada5fb1d9b00
Maria Del Pilar Pena Soto <mpena-so@c3r7s6.42madrid.com>
2023-12-22 19:52:53 +0100

    pruebas de inicio

---
+++ main.c
@@ -0,0 +1,10 @@
+//fd: archivo a leer
+//buffer: Ubicación de almacenamiento de los datos.
+//Número máximo de bytes que se pueden leer.
+
+//info importante read: https://learn.microsoft.com/es-es/cpp/c-runtime-library/reference/read?view=msvc-170
+
+int read( int const fd, void * const buffer, unsigned const buffer_size)
+{
+
+}
