111ddbb0881389b3f573c2e61e84661059772fe4
MPPS90 <mp.pesoto@gmail.com>
2023-12-27 13:52:23 +0100

    pruebas del read

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 //fd: archivo a leer
 //buffer: Ubicación de almacenamiento de los datos.
 //Número máximo de bytes que se pueden leer.
+//read del fd 4 en el buffer 5 bytes cada vez que se llama a la función
+//read puede recordar la posición actual dentro de el archivo (cada vez que lee?)
 
 //info importante read: https://learn.microsoft.com/es-es/cpp/c-runtime-library/reference/read?view=msvc-170
 
-int read( int const fd, void * const buffer, unsigned const buffer_size)
+ssize_t read (int fd, void *buf, size_t nbytes)
 {
 
+    return;
 }
