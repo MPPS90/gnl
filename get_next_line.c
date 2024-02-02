a611cb4b6b074a2fa09dc3c4b2fee046a7cc27d1
Maria Del Pilar Pena Soto <mpena-so@c2r4s5.42madrid.com>
2024-02-02 21:24:16 +0100

    toqueando el while

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: mpena-so <mpena-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 18:04:56 by mpena-so          #+#    #+#             */
-/*   Updated: 2024/02/01 19:15:50 by mpena-so         ###   ########.fr       */
+/*   Updated: 2024/02/02 21:20:37 by mpena-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char    *find(int fd)
 {
     static char *read_line;
     char    *buffer;
@@ -20,35 +20,37 @@ char    *get_next_line(int fd)
     char    *line;
     
     //para la primera lectura 
-    //line = (char *)ft_calloc((strlen(buffer), sizeof(char)));
+    //line = no tengo que reservarle memoria porque apunta a lo que ya tiene apuntado read_line (es decir quee esa memoria ya se ha reservado)
     read_line = malloc(1);
     num_bytes = 1;
+    buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+    if(buffer == NULL)
+        return (NULL);
     while(num_bytes > 0)
     {
-        buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-        if(buffer == NULL)
-            return (NULL);
         num_bytes = read(fd, buffer, BUFFER_SIZE);
         line = read_line;
         read_line = ft_strjoin(read_line, buffer);
-        free(buffer);
-        buffer = NULL;
         free(line);
         line = NULL;
+        printf("->%s<-\n", read_line);
         if(ft_strrchr(read_line, '\n'))
         {
             break;
         }
-    }   
-        // line = read_line;
+    }  
+    free(buffer);
+    buffer = NULL;
+    
 
     printf("NUM BYTES: %zd\n", num_bytes);
     printf("BUF:%s\n", buffer);
     printf("read_line:%s\n", read_line);
-    //printf("TAM BUFFER %lu\n", strlen(buffer));
     printf("TAM BUFFER_SIZE %d\n", BUFFER_SIZE);
-    return(buffer);
-    //free(buffer);
+    return(read_line);   
+}
+char    *get_next_line(int fd)
+{
     
 }
  
