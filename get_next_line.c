3cc3cb461bfccb849be891b838c617f8b03f0093
MPPS90 <mp.pesoto@gmail.com>
2024-04-01 18:17:53 +0200

    se modifica por descomentar el main que lo metí en el doc gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mpena-so <mpena-so@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 18:04:56 by mpena-so          #+#    #+#             */
-/*   Updated: 2024/03/31 15:29:07 by mpena-so         ###   ########.fr       */
+/*   Updated: 2024/04/01 17:23:44 by mpena-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,13 +109,13 @@ char	*get_next_line(int fd)
 	return (final_line);
 }
 
-/* int	main(void)
+int	main(void)
 {
 	int		fd;
 	char	*line;
 
 	
-	fd = open("lord_rings.txt", O_RDONLY);
+	fd = open("gnl/lord_rings.txt", O_RDONLY);
 	line = get_next_line(fd);
 	while (line)
 	{
@@ -127,4 +127,4 @@ char	*get_next_line(int fd)
 	printf("\n\n");
 	close(fd);
 	return (0);
-} */
+}
